ba2f8b57087160a3d24d998b2ac45b2abd419bd8
cerovacmatija <matija.cerovac4@gmail.com>
2024-04-16 15:02:36 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number1, number2, result = 0;
+
+string operation, exit;
+
+Console.WriteLine("Hello, welcome to the calculator program!");
+do
+{
+    Console.WriteLine("Please enter your first number.");
+    number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Please enter your second number.");
+    number2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("What type of operation would you like to do?");
+    Console.WriteLine("a for addition, s for subtraction, m for multiplication, d for division or type e to exit the program.");
+
+    operation = Console.ReadLine();
+
+    if (operation == "a")
+    {
+        result = number1 + number2;
+    }
+    else if (operation == "s")
+    {
+        result = number1 - number2;
+    }
+    else if (operation == "m")
+    {
+        result = number1 * number2;
+    }
+    else if (operation == "d")
+    {
+        result = number1 / number2;
+    }
+    else
+    {
+        Console.WriteLine("Wrong input...");
+    }
+
+    Console.WriteLine("The result is " + result);
+    Console.WriteLine("Would you like to do another calculation?");
+    Console.WriteLine("Type y to continue or n to exit the application.");
+
+    exit = Console.ReadLine();
+
+} while (exit != "n");
+
+Console.WriteLine("The result is " + result);
+Console.WriteLine("Thank you for using the calculator program!");
